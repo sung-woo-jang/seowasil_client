5411b16c29dc12056616ce0c849f81866f9c0418
gyomdyung <seastory624@gmail.com>
2023-02-08 04:07:06 +0900

    Style: AppBar 컴포넌트 추가

--- src/Layout/index.jsx
+++ src/Layout/index.jsx
@@ -1,10 +1,12 @@
 import { Container } from '@mui/material';
 import { Outlet } from 'react-router-dom';
+import AppBar from '../components/AppBar';
 
 const Layout = (props) => {
     return (
         <>
             <Container fixed>
+                <AppBar />
                 <main>
                     <Outlet />
                 </main>
