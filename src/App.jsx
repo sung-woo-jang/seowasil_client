f44667d882313db405f289436b57917dd649d476
gyomdyung <seastory624@gmail.com>
2023-02-08 16:30:14 +0900

    Style: 회원가입 페이지 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import Layout from './Layout';
 import Main from './page/Main';
+import SignUp from './page/SignUp';
 
 function App() {
     return (
         <Routes>
             <Route element={<Layout />}>
                 <Route path="/" element={<Main />} />
+                <Route path="/signup" element={<SignUp />} />
             </Route>
         </Routes>
     );
---
+++ src/page/SignUp/index.jsx
@@ -0,0 +1,98 @@
+import Avatar from '@mui/material/Avatar';
+import {
+    Button,
+    CssBaseline,
+    TextField,
+    Grid,
+    Box,
+    Typography,
+    Container,
+} from '@mui/material';
+import { LockOutlined } from '@mui/icons-material';
+
+export default function SignUp() {
+    return (
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <Box
+                sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+                <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                    <LockOutlined />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    회원가입
+                </Typography>
+                <Box component="form" sx={{ mt: 3 }}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                fullWidth
+                                id="아이디"
+                                label="아이디"
+                                name="아이디"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                fullWidth
+                                name="비밀번호"
+                                label="비밀번호"
+                                type="password"
+                                id="password"
+                                autoComplete="new-password"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                fullWidth
+                                name="비밀번호 확인"
+                                label="비밀번호 확인"
+                                type="password"
+                                id="password-correct"
+                                autoComplete="new-password"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                autoComplete="given-name"
+                                name="firstName"
+                                required
+                                fullWidth
+                                id="firstName"
+                                label="이름"
+                                autoFocus
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                fullWidth
+                                id="email"
+                                label="이메일"
+                                name="email"
+                                autoComplete="email"
+                            />
+                        </Grid>
+                    </Grid>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                    >
+                        가입하기
+                    </Button>
+                </Box>
+            </Box>
+        </Container>
+    );
+}
