4ff14514bd06d5b2933976dec527fc4f39d73c56
gyomdyung <seastory624@gmail.com>
2023-02-23 06:13:59 +0900

    Feat: Lazy Loading 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,64 @@
 import { Route, Routes } from 'react-router-dom';
-import Layout from './Layout';
-import Main from './page/Main';
-import Login from './page/Login';
-import SignUp from './page/SignUp';
-import Product from './page/Product';
-import Category from './page/Category';
-import Introduction from './page/Introduction';
-import Notice from './page/Notice';
-import CustomerCenter from './page/CustomerCenter';
-import NotFound from './page/NotFound';
-import AdminLayout from './Layout/AdminLayout';
-import DashBoard from './page/DashBoard';
-import ManageProduct from './page/ManageProduct';
-import Write from './page/ManageProduct/Write';
-import ManageCustomer from './page/ManageCustomer';
 import axios from 'axios';
 import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import React, { useEffect, Suspense } from 'react';
 import { loginCheck } from './store/slice/authSlice';
-import CustomerCenterWrite from './page/CustomerCenter/CustomerCenterWrite';
 import { AppDispatch } from './store';
-import CustomerCenterDetail from './page/CustomerCenter/CustomerCenterDetail';
+import LoadingSpinner from './components/LoadingSpinner';
 
 axios.defaults.withCredentials = true; // refreshToken cookie를 주고받기 위함
 
+const Layout = React.lazy(() => import('./Layout'));
+const Main = React.lazy(() => import('./page/Main'));
+const Login = React.lazy(() => import('./page/Login'));
+const SignUp = React.lazy(() => import('./page/SignUp'));
+const Product = React.lazy(() => import('./page/Product'));
+const Category = React.lazy(() => import('./page/Category'));
+const Introduction = React.lazy(() => import('./page/Introduction'));
+const Notice = React.lazy(() => import('./page/Notice'));
+const CustomerCenter = React.lazy(() => import('./page/CustomerCenter'));
+const NotFound = React.lazy(() => import('./page/NotFound'));
+const AdminLayout = React.lazy(() => import('./Layout/AdminLayout'));
+const DashBoard = React.lazy(() => import('./page/DashBoard'));
+const ManageProduct = React.lazy(() => import('./page/ManageProduct'));
+const Write = React.lazy(() => import('./page/ManageProduct/Write'));
+const ManageCustomer = React.lazy(() => import('./page/ManageCustomer'));
+const CustomerCenterWrite = React.lazy(() => import('./page/CustomerCenter/CustomerCenterWrite'));
+const CustomerCenterDetail = React.lazy(() => import('./page/CustomerCenter/CustomerCenterDetail'));
+
 function App() {
     const dispatch = useDispatch<AppDispatch>();
-
     useEffect(() => {
         dispatch(loginCheck());
     }, [dispatch]);
 
     return (
-        <Routes>
-            <Route element={<Layout />}>
-                <Route path="" element={<Main />} />
-                <Route path="login" element={<Login />} />
-                <Route path="signup" element={<SignUp />} />
-                <Route path="product/:product_id" element={<Product />} />
-                <Route path="category/:category_id" element={<Category />} />
-                <Route path="introduction" element={<Introduction />} />
-                <Route path="notice" element={<Notice />} />
-                <Route path="customer_center" element={<CustomerCenter />} />
-                <Route path="customer_center/:id" element={<CustomerCenterDetail />} />
-                <Route path="customer_center/write" element={<CustomerCenterWrite />} />
-            </Route>
+        <div>
+            <Suspense fallback={<LoadingSpinner />}>
+                <Routes>
+                    <Route element={<Layout />}>
+                        <Route path="/" element={<Main />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/signup" element={<SignUp />} />
+                        <Route path="/product/:product_id" element={<Product />} />
+                        <Route path="/category/:category_id" element={<Category />} />
+                        <Route path="/introduction" element={<Introduction />} />
+                        <Route path="/notice" element={<Notice />} />
+                        <Route path="/customer_center" element={<CustomerCenter />} />
+                        <Route path="/customer_center/:id" element={<CustomerCenterDetail />} />
+                        <Route path="/customer_center/write" element={<CustomerCenterWrite />} />
+                    </Route>
 
-            <Route path="admin" element={<AdminLayout />}>
-                <Route path="" element={<DashBoard />} />
-                <Route path="product" element={<ManageProduct />} />
-                <Route path="product/write" element={<Write />} />
-                <Route path="customer" element={<ManageCustomer />} />
-            </Route>
-            <Route path="*" element={<NotFound />} />
-        </Routes>
+                    <Route path={`/admin`} element={<AdminLayout />}>
+                        <Route path="" element={<DashBoard />} />
+                        <Route path="product" element={<ManageProduct />} />
+                        <Route path="product/write" element={<Write />} />
+                        <Route path="customer" element={<ManageCustomer />} />
+                    </Route>
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </Suspense>
+        </div>
     );
 }
 
