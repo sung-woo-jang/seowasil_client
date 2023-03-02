5a9ea6020b76861e1780f80abbc5ab006b221f95
gyomdyung <seastory624@gmail.com>
2023-03-03 01:09:16 +0900

    Feat: GlobalStyle 적용

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,8 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
-import './index.css';
 import store from './store';
+import GlobalStyle from './styles/GlobalStyle';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
@@ -13,6 +13,7 @@ root.render(
     <Provider store={store}>
         <CookiesProvider>
             <BrowserRouter>
+                <GlobalStyle />
                 <App />
             </BrowserRouter>
         </CookiesProvider>
---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,30 @@
+import { createGlobalStyle, css } from 'styled-components';
+import reset from 'styled-reset';
+import Colors from './Colors';
+
+const GlobalStyle = createGlobalStyle`
+  ${css`
+      ${reset}
+      @import url('https://fonts.googleapis.com/css2?family=Do+Hyeon&family=Encode+Sans+Expanded:wght@400;700&display=swap');
+
+      @import url('https://fonts.googleapis.com/css2?family=Gothic+A1:wght@100;200;300;400;500;600;700;800;900&display=swap');
+
+      @import url('https://fonts.googleapis.com/css2?family=Gothic+A1:wght@200;300;400;500;600;700;800;900&family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap');
+
+      * {
+          margin: 0;
+          padding: 0;
+          box-sizing: border-box;
+          font-family: 'Gothic A1', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto',
+              'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+              sans-serif;
+          -webkit-font-smoothing: antialiased;
+          -moz-osx-font-smoothing: grayscale;
+          text-decoration: none;
+          list-style: none;
+          color: ${Colors.Black};
+      }
+  `}
+`;
+
+export default GlobalStyle;
