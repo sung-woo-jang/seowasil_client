5316c44156508e4cd2573187356a1dc2e327ae63
gyomdyung <seastory624@gmail.com>
2023-03-28 02:04:49 +0900

    Feat: 액세스 토큰 자동 재발급

--- src/utils/api/index.ts
+++ src/utils/api/index.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosInstance, AxiosResponse } from 'axios';
 
 export const instance: AxiosInstance = axios.create({
     baseURL: process.env.REACT_APP_API_URL,
@@ -7,6 +7,21 @@ export const instance: AxiosInstance = axios.create({
         'Content-Type': 'application/json',
     },
 });
+
+instance.interceptors.response.use(
+    (response: AxiosResponse) => response,
+    (error) => {
+        // AccessToken이 만료된 경우, RefreshToken으로 AccessToken을 갱신하고 특정 API를 요청함
+        if (error.response && error.response.status === 403)
+            return instance
+                .get('/users/refresh')
+                .then((res) => {})
+                .catch((error) => {
+                    return Promise.reject(error);
+                });
+        else return Promise.reject(error);
+    },
+);
 
 export const formInstance = axios.create({
     baseURL: process.env.REACT_APP_API_URL,
