aa74beeb733acc1c6c7064095709b65847de66dd
gyomdyung <54757435+sung-woo-jang@users.noreply.github.com>
2024-04-20 17:21:16 +0900

    2024년 4월 20일 토요일 17시 21분 16초 KST

--- api/axiosInstance.ts
+++ api/axiosInstance.ts
@@ -1,9 +1,18 @@
 import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
 import { API_URL } from '../constants/API_URL';
 
+/* 
+개발 환경
+- baseUrl : localhost:8000
+process.env.NODE_ENV === development
+
+배포 환경
+- baseUrl : seowasil.shop/api
+process.env.NODE_ENV === production
+*/
 const createAxiosInstance = (contentType: string): AxiosInstance => {
   const config: AxiosRequestConfig = {
-    baseURL: `http://localhost:8000`,
+    baseURL: API_URL.BASE_URL,
     withCredentials: true,
     headers: {
       'Content-Type': contentType,
